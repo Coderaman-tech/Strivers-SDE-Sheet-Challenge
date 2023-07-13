081c926f33a777bb3f4e3d82e4ed0ba54a820301
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2023-07-14 00:35:03 +0530

    Create Pow(x, n).cpp

---
+++ Arrays Part-III/Pow(x, n).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+   double func(double x,long long n,double ans){
+       if(n==0)
+       return 1;
+       if(n%2==0){
+          double b=func(x,n/2,ans);
+          ans=b*b;
+          return ans;
+       }
+       else{
+          double b=func(x,n-1,ans);
+          ans= x*b;
+          return ans;
+       }
+       return ans;
+   }
+    double myPow(double x, int n) {
+        double ans=0.0;
+         long long a=n;
+        if(n<0){
+           
+            return 1/func(x,-1*a,ans);
+        }
+        else
+        return func(x,a,ans);
+    }
+};
