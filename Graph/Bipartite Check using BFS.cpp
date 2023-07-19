4d919aae57f05f0e033f52632be69dcd0fb38544
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2023-07-20 02:34:52 +0530

    Create Bipartite Check using BFS.cpp

---
+++ Graph/Bipartite Check using BFS.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+bool isGraphBirpatite(vector<vector<int>> &edges) {
+	// Write your code here.
+	int n=edges.size();
+	vector<int>adj[n];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(edges[i][j]==1){
+				adj[i].push_back(j);
+				adj[j].push_back(i);
+			}
+		}
+	}
+
+	vector<int>col(n,-1);
+	queue<int>q;
+	q.push(0);
+	col[0]=0;
+
+	while(!q.empty()){
+		int node=q.front();
+		q.pop();
+		for(auto it:adj[node]){
+			if(col[it]==-1){
+				col[it]=!col[node];
+				q.push(it);
+			}
+			else{
+				if(col[it]==col[node])
+				return false;
+			}
+		}
+	}
+    return true;
+}
